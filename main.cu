19248a107ab73288ba46bd4d3bf44239f7e78389
Dominik Ernst <dominik.ernst@fau.de>
2019-06-19 08:36:40 -0400

    inital commit, OpenMP parallelized CPU code

---
+++ main.cu
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+typedef double DTYPE;
+
+int main(int argc, char **argv) {
+  const int width = 400;
+  const int height = 400;
+  const int iters = 10000;
+  DTYPE *gridA = new DTYPE[width * height];
+  DTYPE *gridB = new DTYPE[width * height];
+
+  #pragma omp parallel for
+  for (int y = 0; y < height; y++) {
+    for (int x = 0; x < width; x++) {
+      if (y != 0) {
+        gridA[y * width + x] = 0.0;
+        gridB[y * width + x] = 0.0;
+      } else {
+        gridA[y * width + x] = 1.0;
+        gridB[y * width + x] = 1.0;
+      }
+    }
+  }
+
+  for (int it = 0; it < iters; it++) {
+
+    if (it % 10 == 0) {
+      DTYPE residual = 0.0;
+#pragma omp parallel for reduction(+:residual)
+      for (int y = 1; y < height - 1; y++) {
+        for (int x = 1; x < width - 1; x++) {
+          residual += -4.0 * gridB[y * width + x] +
+                      (gridB[(y + 1) * width + x] + gridB[(y - 1) * width + x] +
+                       gridB[y * width + x - 1] + gridB[y * width + x + 1]);
+        }
+      }
+      cout << it << " " << residual << "\n";
+
+#pragma omp parallel for
+      for (int y = 1; y < height-1; y++) {
+        for (int x = 1; x < width-1; x++) {
+          gridA[y * width + x] =
+              0.25 * (gridB[(y + 1) * width + x] + gridB[(y - 1) * width + x] +
+                      gridB[y * width + x - 1] + gridB[y * width + x + 1]);
+        }
+      }
+
+      swap(gridA, gridB);
+    }
+  }
+
+  delete gridA;
+  delete gridB;
+
+  return 0;
+}
